023db465976c137b105f0b0de6b9aace5222767d
Krish Patel <krishpatel0512@gmail.com>
2024-12-25 15:43:56 -0700

    Created empty board
    Created an empty board for the game along with the border, starting to create the lines for the grid

--- main.py
+++ main.py
@@ -5,21 +5,43 @@ Created December 2024
 '''
 
 import pygame
+from hupper import start_reloader #not needed for this project, auto updates GUI as changes are made
 
-pygame.init()
+def main():
+    pygame.init()
 
-# Window Setup
-SCREEN_WIDTH = 720
-SCREEN_HEIGHT = 720
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Krish Minesweeper")
+    ''' Window Setup '''
+    SCREEN_WIDTH = 720
+    SCREEN_HEIGHT = 720
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Krish Minesweeper")
 
-# Loop
-run = True
-while run:
-    
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+    board = pygame.Rect((10, 100, 700, 610)) #board for the game
+    border = pygame.Rect((5, 95, 710, 620)) #border for game
 
-pygame.quit()
+    #Color variables
+    hidden_color = (160, 160, 160)
+    revealed_color = (96, 96, 96)
+    BLACK = (0, 0, 0)
+
+    ''' Loop '''
+    run = True
+    while run:
+
+        screen.fill((250, 250, 250)) #background color white
+        pygame.draw.rect(screen, hidden_color, board)
+        pygame.draw.rect(screen, BLACK, border, width = 5)
+
+        ''' Event Handler '''
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        pygame.display.update()
+
+    pygame.quit()
+
+'''Refreshes the GUI as you code, also not needed for project'''
+if __name__ == "__main__":
+    reloader = start_reloader('main.main')
+    main()
